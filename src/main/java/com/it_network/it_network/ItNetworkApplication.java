54d568d114d88d6c0eb6229c97f74da3aeaaa9e1
이재훈 <ijaehun@ijaehuns-MacBook-Air.local>
2025-09-19 17:30:35 +0900

    spring setting commit

---
+++ src/main/java/com/it_network/it_network/ItNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.it_network.it_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/it_network/it_network/ItNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it_network.it_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
